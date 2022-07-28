2b996b01cee0443e0704ec1fa54b29e5d3a4e28d
EmmanuelD79 <e.demeulnaere@gmail.com>
2022-07-28 19:23:56 +0200

    modification vue signup

--- LITReview/authentication/forms.py
+++ LITReview/authentication/forms.py
@@ -5,12 +5,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
 
-    username = forms.CharField(max_length=63, label="Nom d'utilisateur")
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label="Mot de passe")
+    username = forms.CharField(max_length=63, label="Nom d'utilisateur", help_text="Saisir votre nom d'utilisateur")
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label="Mot de passe", help_text="Saisir votre mot de passe")
 
 
 class SignupForm(UserCreationForm):
 
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'field-witdh', 'placeholder': "Nom d'utilisateur" }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'field-witdh', 'placeholder': "Mot de passe" }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'field-witdh', 'placeholder': "Confirmer mot de passe" }))
+
     class Meta(UserCreationForm):
         model = get_user_model()
-        fields = ('username', 'role')
+        fields = ('username',)
